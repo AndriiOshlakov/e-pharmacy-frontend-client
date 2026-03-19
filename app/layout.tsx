b5ec4e93314b6af74eea7af76d779052672bff13
Andrii <andreyoshlakov18051986@gmail.com>
2026-03-19 14:11:35 +0200

    deleted modal from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={`${inter.variable} `}>
